1375385edfd6de1e7c54656d5b50003fdc93ce86
Stephen Lalor <stephen.lalor.work@gmail.com>
2018-08-26 16:12:33 +0100

    WIP - API functions, separately.

--- API_test.R
+++ API_test.R
@@ -2,21 +2,17 @@
 ##########==================================== API TEST ===============================================##########
 #===============================================================================================================#
 
-#=======================================================#
+#=================================================================#
 #                     LOAD PACKAGES
-#=======================================================#
+#=================================================================#
 
-#testtesttest
 library(tidyverse)
 library(httr)
 library(jsonlite)
 
-#=======================================================#
-#                     BUILD REQUEST
-#=======================================================#
-
-#Example:
-##https://simfin.com/api/v1/companies/id/{companyId}
+#=================================================================#
+#                     SETUP FOR TESTING
+#=================================================================#
 
 #API key:
 api_key <- "gt14fI6OUddLNfBbRLzZVN3726imMqEk" #Obtained from: https://simfin.com/data/access/api.
@@ -24,60 +20,161 @@ api_key <- "gt14fI6OUddLNfBbRLzZVN3726imMqEk" #Obtained from: https://simfin.com
 #Define root url:
 url_api_root <- "https://simfin.com/api/v1/companies/id/"
 
-#Build request:
-GET_BY_SFID <- function(sfid, type, api_key){
-  #### Returns data of "type" for a given SimFin ID "sfid". ####
-  
-  #Define url root extensions for data return type:
-  url_extn_list <- list("available_statements" = "/statements/list/",
-                        "statements_orig" = "/statements/original/",
-                        "statements_stan" = "/statements/standardised/",
-                        "ratios" = "/ratios/",
-                        "shares_agg" = "/shares/aggregated/",
-                        "share_price" = "/shares/prices/",
-                        "share_classes" = "/shares/classes/list/") 
-  
-  #Put together request URL:
-  request_url <- paste0(url_api_root, sfid, url_extn_list[[type]], "?api-key=", api_key)
-
-  #Perform request:
-  cat("Performing request for SFID:", sfid, type, "...", "\n",
-      "\t", "URL: ", request_url, "\n", "\n")
-
-  start_time <- Sys.time() #Starting time.
-  request <- GET(request_url) #Request.
-  finish_time <- Sys.time() #Ending time.
-  request_status <- status_code(request) #Request status code.
-
-  cat("Request for", sfid, " :complete.", "\n",
-        "\t", "Status code: ", request_status, "\n",
-        "\t", "Request duration: ", finish_time - start_time,  "\n", "\n")
-
-  #Return result:
-  return(request)
-}
 
 #Example request:
-request_1 <- GET_BY_SFID(sfid = "65735", type = "ratios", api_key)
+eg_simfin_id <- "65735"
 
-#=======================================================#
-#                     EXAMINE RESPONSE
-#=======================================================#
+#=================================================================#
+#                     GET SIMFIN ID BY TICKER ##WORKING
+#=================================================================#
 
-#Examine response components:
-names(request_1)
+GET_SFID_BY_TICKER <- function(ticker, api_key){
+  #### Finds SimFin ID associated with ticker. ####
+  
+  #Construct request URL:
+  url_api_root <- "https://simfin.com/api/v1/info/find-id/ticker/" #Define root url for this query.
+  request_url <- paste0(url_api_root, ticker, "?api-key=", api_key)
+  
+  #Perform request:
+  cat("Performing request for TICKER:", ticker, "...", "\n",
+      "\t", "URL: ", request_url, "\n", "\n")
+  
+  start_time <- Sys.time() #Starting time.
+  response <- GET(request_url) #Request.
+  finish_time <- Sys.time() #Ending time.
+  request_status <- status_code(response) #Request status code.
+  
+  cat("Request for", ticker, ": complete.", "\n",
+      "\t", "Status code: ", request_status, "\n",
+      "\t", "Request took: ", finish_time - start_time,  "\n", "\n")
+  
+  #Extract content:
+  response_content <- content(response)
+  if(request_status == 400){ #Stop process if error:
+    stop(paste0("Error code: 400! Bad request: ", response_content[[1]]))
+  }
+  
+  #Format response content:
+  cat("Processing response content...")
+  for(i in 1:length(response_content)){
+    
+    #Initialise dataframe:
+    if(i == 1){ #On first loop...
+      response_colnames <- names(response_content[[i]]) #Extract colnames.
+      response_df <- data.frame(matrix(nrow = 0, ncol = length(response_colnames))) #Intialise the data frame.
+      response_df <- response_df %>% mutate_all(as.character) #Change all to character, avoid errors to do with factors.
+    }
+    
+    #Extract data from request content:
+    vals <- as.character(response_content[[i]]) #Values corresponding to those names.
+    
+    #Assign current row of data to DF:
+    response_df <- rbind(response_df, vals)
+    response_df <- response_df %>% mutate_all(as.character) #Change all to character, avoid errors to do with factors.
+    
+    #Final formatting:
+    if(i == length(response_content)){
+      names(response_df) <- names(response_content[[i]]) #Assign names of columns.
+      response_df[response_df == "NULL"] <- NA
+    }
+  }
+  cat("Processing complete.")
+  
+  #Return response data content:
+  return(response_df)
+}
 
-#Process API request content:
-request_1_content <- content(request_1)
+#Example:
+example_1 <- GET_SFID_BY_TICKER("AAPL", api_key)
 
-#Apply function across all list elements to extract the name and address of each repo:
-request_1_df <- lapply(request_1_content,
-                       function(x){
-                         df <- data_frame(INDICATOR_ID = x$name,
-                                          INDICATOR_NAME = x$html_url,
-                                          VALUE = x$git_commits_url,
-                                          PERIOD = x$period,
-                                          F_YEAR = x$fyear,
-                                          CURRENCY = x$currency,
-                                          PERIOD_END_DATE = x$`period-end-date`)
-                         }) %>% bind_rows()
+#=================================================================#
+#                     GET GENERAL INFO BY SIMFIN ID ##WORKING
+#=================================================================#
+
+GET_INFO_BY_SFID <- function(sfid, api_key){
+  #### Finds company general info by ticker. ####
+  
+  #Construct request URL:
+  url_api_root <- "https://simfin.com/api/v1/companies/id/" #Define root url for this query.
+  request_url <- paste0(url_api_root, sfid, "?api-key=", api_key)
+  
+  #Perform request:
+  cat("Performing request for SFID:", sfid, "...", "\n",
+      "\t", "URL: ", request_url, "\n", "\n")
+  
+  start_time <- Sys.time() #Starting time.
+  response <- GET(request_url) #Request.
+  finish_time <- Sys.time() #Ending time.
+  request_status <- status_code(response) #Request status code.
+  
+  
+  cat("Request for", sfid, ": complete.", "\n",
+      "\t", "Status code: ", request_status, "\n",
+      "\t", "Request took: ", finish_time - start_time,  "\n", "\n")
+  
+  #Extract content:
+  response_content <- content(response)
+  if(request_status == 400){ #Stop process if error:
+    stop(paste0("Error code: 400! Bad request: ", response_content[[1]]))
+  }
+  
+  #Format response content:
+  cat("Processing response content...")
+  
+  response_colnames <- names(response_content) #Extract colnames.
+  response_df <- data.frame(matrix(nrow = 0, ncol = length(response_colnames))) #Intialise the data frame.
+  response_df <- response_df %>% mutate_all(as.character) #Change all to character, avoid errors to do with factors.
+  vals <- as.character(response_content) #Extract data. Values corresponding to names.
+  response_df <- rbind(response_df, vals)
+  response_df <- response_df %>% mutate_all(as.character) #Change all to character, avoid errors to do with factors.
+  names(response_df) <- names(response_content) #Assign names of columns.
+  response_df[response_df == "NULL"] <- NA #Deal with NULLs.
+  
+  cat("Processing complete.")
+  
+  #Return response data content:
+  return(response_df)
+}
+
+#Example:
+example_2 <- GET_INFO_BY_SFID("65735", api_key)
+
+#=================================================================#
+#                     GET GENERAL INFO BY SIMFIN ID ##WIP
+#=================================================================#
+#https://simfin.com/api/v1/companies/id/{companyId}/statements/standardised
+
+GET_STATMENTS_BY_SFID <- function(sfid, api_key, fyear,
+                                  stype = c("pl", "bs", "cf"), 
+                                  ptype = c("TTM", "TTM-1", "TTM-2", "Q1", "Q2", "Q3", "Q4", "H1", "H2", "9M", "FY")
+                                  ){
+  #### Returns company statements as DF, by SFID. ####
+  
+  #Evaluate argument choice:
+  stype <- match.arg(stype)
+  ptype <- match.arg(ptype)
+
+  #Construct request URL:
+  url_api_root <- "https://simfin.com/api/v1/companies/id/" #Define root url for this query.
+  request_url <- paste0(url_api_root, sfid, "/statements/standardised", "?api-key=", api_key)
+  
+  #Perform request:
+  cat("Performing request for SFID:", sfid, "...", "\n",
+      "\t", "URL: ", request_url, "\n", "\n")
+  
+  start_time <- Sys.time() #Starting time.
+  response <- GET(request_url) #Request.
+  finish_time <- Sys.time() #Ending time.
+  request_status <- status_code(response) #Request status code.
+  
+  
+  cat("Request for", sfid, ": complete.", "\n",
+      "\t", "Status code: ", request_status, "\n",
+      "\t", "Request took: ", finish_time - start_time,  "\n", "\n")
+  
+  #Extract content:
+  response_content <- content(response)
+  if(request_status == 400){ #Stop process if error:
+    stop(paste0("Error code: 400! Bad request: ", response_content[[1]]))
+  }
+}
